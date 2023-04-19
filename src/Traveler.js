2b212c3cce974e0ea1476b17f2b75da8fb9ee454
Sharie Trachsel <strachsel22@turing.edu>
2023-04-19 12:19:56 -0500

    Add Traveler class code and minor fixes to Travler-test

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,5 +1,15 @@
 class Traveler {
+constructor(travelerInfo){
+    this.travelerID = travelerInfo.id;
+    this.travelerName =travelerInfo.name;
+    this.travelerType = travelerInfo.travelerType;
+}
 
+findFirstName(){
+    const names = this.travelerName.split(' ')
+
+    return names[0]
+}
 }
 
 export default Traveler;
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -10,28 +10,28 @@ describe('Traveler', function () {
     
 
     beforeEach('data creation', () => {
-        // testRepository = new TravelerRepository(sampleTravelers);
-        // testTraveler = new Traveler(testRepository.findTravelerById(4))
+        testRepository = new TravelerRepository(sampleTravelers);
+        testTraveler = new Traveler(testRepository.findTravelerById(4))
     });
 
-    it.skip('should be a function', function () {
+    it('should be a function', function () {
         expect(Traveler).to.be.a('function');
     });
 
-    it.skip('should be an instance of Traveler', function () {
+    it('should be an instance of Traveler', function () {
         expect(testTraveler).to.be.an.instanceof(Traveler);
     });
 
-    it.skip('should store store a travlers information', function () {
+    it('should store store a travelers information', function () {
         expect(testTraveler.travelerID).to.be.equal(4);
         expect(testTraveler.travelerName).to.be.equal('Leila Thebeaud');
         expect(testTraveler.travelerType).to.be.equal('photographer');
     });
 
-    it.skip('should be able to find the first name of the travler', function () {
+    it('should be able to find the first name of the travler', function () {
         expect(testTraveler.findFirstName()).to.be.equal('Leila');
 
-        let testTraveler2 = new Traveler(repository.findTravelerById(2));
+        let testTraveler2 = new Traveler(testRepository.findTravelerById(2));
 
         expect(testTraveler2.findFirstName()).to.be.equal('Rachael');
 
