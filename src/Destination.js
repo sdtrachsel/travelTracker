b694635fcc956231a7a50a01e06383bf8c98065c
Sharie Trachsel <strachsel22@turing.edu>
2023-04-19 12:33:25 -0500

    Add Destination class code and small fixes to Destination-test

--- src/Destination.js
+++ src/Destination.js
@@ -1,5 +1,21 @@
 class Destination {
+    constructor(destinationList) {
+        this.allDestinations = destinationList;
+    }
 
+    findById(id) {
+        return this.allDestinations.find(destination => destination.id === id);
+    }
+
+    calculateDestinationCost(id, numTavelers, duration) {
+        const destination = this.allDestinations.find(destination => destination.id === id);
+
+        const cost = (numTavelers * destination.estimatedFlightCostPerPerson) + (duration * destination.estimatedLodgingCostPerDay);
+
+        const totalCost = cost * 1.1;
+
+        return totalCost;
+    };
 }
 
 export default Destination;
