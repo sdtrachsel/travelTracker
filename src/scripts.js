3bf371ea64c128411e160c9e8798564023577085
Sharie Trachsel <strachsel22@turing.edu>
2023-04-19 19:54:40 -0500

    Add basic css

--- src/scripts.js
+++ src/scripts.js
@@ -7,7 +7,7 @@ import Trip from './Trip'
 import Destination from './Destination';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/user-profile.png'
 
 
 
@@ -16,24 +16,48 @@ const landingPage = document.getElementById('landingDisplay');
 const upcomingPage = document.getElementById('upcomingDisplay');
 const pastPage = document.getElementById('pastDisplay');
 const desinationPage = document.getElementById('destinationsDisplay');
-const bookTripPage = document.getElementById('bookTripDisplay');
+const planTripPage = document.getElementById('planTripDisplay');
+const viewHomeBtn = document.getElementById('viewHomeBtn');
+const viewPastBtn = document.getElementById('viewPastBtn');
+const viewPlanTripBtn = document.getElementById('viewPlanTripBtn');
+const loginName = document.getElementById('userName')
+
 
 let allTravelers;
 let allDestinations;
 let currentUser;
 let currentUserTrips;
 
-// Listeners
+// Eventlisteners
 window.addEventListener('load', () => {
     getLoadData()
         .then(data => {
-            allTravelers = new TravelerRepository (data[0].travelers);
-            allDestinations= new Destination (data[1].destinations);
-            currentUser = new Traveler (allTravelers.findTravelerById(14));
-            currentUserTrips = new Trip (allTravelers.findTravelerById(14), data[2].trips)
+            allTravelers = new TravelerRepository(data[0].travelers);
+            allDestinations = new Destination(data[1].destinations);
+            currentUser = new Traveler(allTravelers.findTravelerById(14));
+            currentUserTrips = new Trip(allTravelers.findTravelerById(14), data[2].trips)
+            populateUponLoad()
         })
         .catch(err => console.log(err))
-
-    
-
 });
+
+viewHomeBtn.addEventListener('click', populateHomePage)
+viewPastBtn.addEventListener('click', populatePastPage)
+viewPlanTripBtn.addEventListener('click', populatePlanTripPage)
+
+
+function populateUponLoad() {
+    loginName.innerText = `${currentUser.travelerName}`
+}
+
+function populateHomePage() {
+    console.log('Home button clicked')
+}
+
+function populatePastPage() {
+    console.log('Past button clicked')
+}
+
+function populatePlanTripPage() {
+    console.log('Plan Trip button clicked')
+}
