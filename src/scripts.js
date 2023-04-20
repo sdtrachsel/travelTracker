51f4908b1e4bfeae055be48387be3d0dcf84659a
Sharie Trachsel <strachsel22@turing.edu>
2023-04-20 10:48:06 -0500

    Add logic for home page

--- src/scripts.js
+++ src/scripts.js
@@ -12,8 +12,7 @@ import './images/user-profile.png'
 
 
 // Selectors
-const landingPage = document.getElementById('landingDisplay');
-const upcomingPage = document.getElementById('upcomingDisplay');
+const homePage = document.getElementById('homeDisplay');
 const pastPage = document.getElementById('pastDisplay');
 const desinationPage = document.getElementById('destinationsDisplay');
 const planTripPage = document.getElementById('planTripDisplay');
@@ -21,7 +20,11 @@ const viewHomeBtn = document.getElementById('viewHomeBtn');
 const viewPastBtn = document.getElementById('viewPastBtn');
 const viewPlanTripBtn = document.getElementById('viewPlanTripBtn');
 const loginName = document.getElementById('userName')
-
+const upComingTripTable = document.getElementById('upcomingTrps')
+const pastTripTable = document.getElementById('pastTrps')
+const userGreeting = document.getElementById('greeting')
+const tripAllTime = document.getElementById('totalSpent')
+const destinationCardDisplay = document.getElementById('destinationCards')
 
 let allTravelers;
 let allDestinations;
@@ -47,17 +50,80 @@ viewPlanTripBtn.addEventListener('click', populatePlanTripPage)
 
 
 function populateUponLoad() {
-    loginName.innerText = `${currentUser.travelerName}`
+    loginName.innerText = `${currentUser.travelerName}`;
+    populateHomePage()
+    populatePastPage()
 }
 
 function populateHomePage() {
-    console.log('Home button clicked')
+    userGreeting.innerText = `Welcome back, ${currentUser.travelerName}.`;
+    createTripsTable(upComingTripTable, currentUserTrips.findByTense('upcoming'));
+    updateAllTimeTripCost();
 }
 
 function populatePastPage() {
-    console.log('Past button clicked')
+    createTripsTable(pastTripTable, currentUserTrips.findByTense('past'))
 }
+
 
 function populatePlanTripPage() {
     console.log('Plan Trip button clicked')
+}
+
+function createTripsTable(table, tripList) {
+    table.innerHTML = ''
+
+    table.innerHTML += `
+        <tr>
+            <th>Status</th>
+            <th>Destination</th>
+            <th>Date</th>
+            <th>Duration</th>
+            <th>Travelers</th>
+         </tr>`;
+
+    tripList.forEach(trip => {
+        table.innerHTML += `
+            <tr>
+                 <td>${trip.status}</td>
+                 <td>${trip.destinationID}</td>
+                 <td>${trip.date}</td>
+                 <td>${trip.duration}</td>
+                <td>${trip.travelers}</td>
+            </tr>`;
+    });
+}
+
+function updateAllTimeTripCost(){
+    const total = currentUserTrips.calulateAllTimeCost(allDestinations)
+
+    tripAllTime.innerText = `You have spent ${numberToDollar(total)} on all your trips.`
+}
+
+function numberToDollar(num){
+    const USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    const dollars = USDollar.format(num);
+
+    return dollars 
+}
+
+function createDestinationCards(destinations){
+    destinationCardDisplay.innerHTML = '';
+
+    destinations.forEach((destination)=> {
+        const destinationLocation = destination.split(',')
+
+        destinationCardDisplay.innerHTML += `
+        <section class="dest-card">
+            <h3>${destinationLocation[0]}</h3>
+            <p>${destinationLocation[1]}</p>
+        </section>
+        `
+        
+    })
+
 }
