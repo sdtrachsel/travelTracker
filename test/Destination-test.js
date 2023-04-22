7478e8dd115672721045c5103de345151777e439
Sharie Trachsel <strachsel22@turing.edu>
2023-04-22 14:01:31 -0500

    Add findByName to destination test

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -43,6 +43,26 @@ describe('Destination', function () {
         });       
     });
 
+    it('should find a destination by name', function () {
+        expect(testDestinations.findByName('Anchorage, Alaska')).to.deep.equal({
+            "id": 35,
+            "destination": "Anchorage, Alaska",
+            "estimatedLodgingCostPerDay": 200,
+            "estimatedFlightCostPerPerson": 100,
+            "image": "https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+            "alt": "man riding on kayak surrounded by mountains"
+        });
+        
+        expect(testDestinations.findByName('Quito, Ecuador')).to.deep.equal({
+            "id": 19,
+            "destination": "Quito, Ecuador",
+            "estimatedLodgingCostPerDay": 60,
+            "estimatedFlightCostPerPerson": 500,
+            "image": "https://images.unsplash.com/photo-1501684691657-cf3012635478?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+            "alt": "a city at night with cloudy, snowy mountains in the distance"
+        });   
+    })
+
     it('should calculate the cost of visiting a destination', function () {
         expect(testDestinations.calculateDestinationCost(35, 1, 10)).to.be.equal(2310);  
         expect(testDestinations.calculateDestinationCost(25, 5, 18)).to.be.equal(4565);          
